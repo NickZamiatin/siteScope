4a5f99d1e6223a95ecaa9a3ddca44bf0e34631ec
Nikita Zamiatin <4406015@gmail.com>
2018-04-03 11:51:40 -0400

    Scope1

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-alert(Hello World )
-
-let name = "Nick";
-let age = 22;
-let color = "red";
+// alert(Hello World )
+//
+// let name = "Nick";
+// let age = 22;
+// let color = "red";
 
 
 
 function myFunc() {
-  let myFuckVar = "hey"
-
+  let fName = prompt("Tipe your first name " );
+  let lName = prompt("Tipe your last name " );
+  console.log(`Welcome to Galvanize ${fName} ${lName}`)
 }
+ myFunc()
